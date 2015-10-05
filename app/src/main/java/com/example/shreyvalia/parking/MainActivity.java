1f88e90631794cdecbf898a76f9abc958fa428a5
Shrey Valia <svalia@ucsc.edu>
2015-10-05 10:34:10 -0700

    new commit

--- app/src/main/java/com/example/shreyvalia/parking/MainActivity.java
+++ app/src/main/java/com/example/shreyvalia/parking/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        System.out.println("first commit");
         setContentView(R.layout.activity_main);
     }
 
