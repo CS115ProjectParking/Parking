6f3d0c6b90363a321331f5b348456f221735222b
Shrey Valia <svalia@ucsc.edu>
2015-11-12 15:08:13 -0800

    really inefficint and shitty, but updates corewest values as firebase gets updated.

--- app/src/main/java/com/example/shreyvalia/parking/LotIntentService.java
+++ app/src/main/java/com/example/shreyvalia/parking/LotIntentService.java
@@ -2,9 +2,13 @@ package com.example.shreyvalia.parking;
 
 import android.app.IntentService;
 import android.content.Intent;
-import android.content.Context;
 import android.content.res.Resources;
 import android.support.v4.content.LocalBroadcastManager;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 /**
  * An {@link IntentService} subclass for handling asynchronous task requests in
@@ -13,13 +17,19 @@ import android.support.v4.content.LocalBroadcastManager;
 
 public class LotIntentService extends IntentService {
 
+    Firebase myFB;
 
     public LotIntentService() {
         super("LotIntentService");
     }
 
+
+
     @Override
     protected void onHandleIntent(Intent intent) {
+        Firebase.setAndroidContext(this);
+        myFB = new Firebase("https://torrid-heat-8415.firebaseio.com/");
+
         if (intent != null) {
             int lot_number = intent.getIntExtra("lot", 0);
             //TODO: get data from server instead of mock data from lots.xml
@@ -29,11 +39,28 @@ public class LotIntentService extends IntentService {
             Resources res = getApplicationContext().getResources();
             int cap = 0, pop = 0;
 
+            final int [] popTemp = {0};
             //get data corresponding to lot number
             switch (lot_number) {
                 case 0:
+                    myFB.child("Count").addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot snapshot) {
+                            String a = snapshot.getValue().toString();
+                            
+                            String b = a.substring(0).toString();
+                            popTemp[0] = Integer.parseInt(b);
+
+                        }
+
+                        @Override
+                        public void onCancelled(FirebaseError error) {
+                        }
+
+                    });
+
                     cap = res.getInteger(R.integer.corewest_cap);
-                    pop = res.getInteger(R.integer.corewest_pop);
+                    pop = popTemp[0];
                     break;
                 case 1:
                     cap = res.getInteger(R.integer.northremote_cap);
@@ -49,7 +76,7 @@ public class LotIntentService extends IntentService {
                     break;
                 case 4:
                     cap = res.getInteger(R.integer.crown_cap);
-                    pop = res.getInteger(R.integer.crown_pop);
+                    pop= res.getInteger(R.integer.crown_pop);
                     break;
                 case 5:
                     cap = res.getInteger(R.integer.healthcenter_cap);
@@ -66,5 +93,20 @@ public class LotIntentService extends IntentService {
             LocalBroadcastManager.getInstance(this).sendBroadcast(statusIntent);
         }
     }
+
+//    private int stringToInt(String a){
+//        Log.e("Main Change Method: '", a);
+////        ": 0202"
+//        String b  = new String();
+//        for(int i = 2; i < a.length(); i++)
+//        {
+////            Log.e("a value i is : ", a.valueOf(i));
+//            b.concat(String.valueOf(a.charAt(i)));
+//        }
+//        Log.e("Main Change b is: '", b);
+//        return 0;
+//
+//
+//    }
 
 }
--- app/src/main/java/com/example/shreyvalia/parking/MainActivity.java
+++ app/src/main/java/com/example/shreyvalia/parking/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.shreyvalia.parking;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,7 +16,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        System.out.println("first commie33");
         setContentView(R.layout.activity_main);
         ImageButton menu_button = (ImageButton) findViewById(R.id.menuImageButton);
         //pop up a Toast on long press
